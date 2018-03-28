2d5930e89f9c4a4f9777d49e1bc073f66d39b0ff
MonseAE <monse110587@gmail.com>
2018-03-28 11:16:14 -0600

    Se agregan tres funciones en el javascript para lograr la funcionalidad del problema a resolver.

--- README.md
+++ README.md
@@ -23,4 +23,4 @@ En esta parte se describe la funcionalidad del proyecto descrito en pasos.
 
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-![Diagrama de flujo del problema SCOREKEEPER](assets\image\diagrama de flujo SCOREKEEPER.png)
+![Diagrama de flujo del problema SCOREKEEPER](assets\image\Diagrama de flujo - Página 1 .png)
--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-alert('hola');
+var scores = {};// Va a llevar el registro de los jugadores
+console.log(scores);
+
+function createPlayer(object, name){
+  object[name.toLowerCase()] = 0;// crea las keys a el objeto
+  console.log(object);
+}
+
+
+function addPoints(name, points){
+  scores[name.toLowerCase()] += points;//asigna los valores a las keys del objeto, va sumando el puntaje al jugador
+  console.log(scores);
+}
+
+function printAllPoints(){
+  var arrayPlayers = Object.keys(scores);
+  var resultados = '';
+
+  for(var k = 0; k < arrayPlayers.length; k++){
+    //resultados += arrayPlayers[k] +': ' + scores[arrayPlayers[k]] +', ';
+    if(k == arrayPlayers.length -1){// se verifica que estemos en la ultima posicion para quitar la coma que esta al final de los elementos
+        resultados += arrayPlayers[k] +': ' + scores[arrayPlayers[k]];
+    }else{//si no esta en la utlima posicion pone la coma
+        resultados += arrayPlayers[k] +': ' + scores[arrayPlayers[k]] +', ';
+    }
+  }
+return resultados;
+}
+
+createPlayer(scores, 'Ana');
+addPoints('ana', 20);
+addPoints('ana', 40);
+createPlayer(scores, 'Silvana');
+addPoints('Silvana',50);
+console.log(printAllPoints());
